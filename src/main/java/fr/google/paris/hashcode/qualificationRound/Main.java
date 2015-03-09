0a56fae88aa2e299459f5efe5d1045f280d61514
Luc CLÉMENT <luc.clement421@gmail.com>
2015-03-09 08:56:44 -0400

    ajout de fichiers oubliés

---
+++ src/main/java/fr/google/paris/hashcode/qualificationRound/Main.java
@@ -0,0 +1,16 @@
+package fr.google.paris.hashcode.qualificationRound;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+
+	private final static Logger LOGGER = Logger.getLogger(Main.class);
+
+	public static void main(String[] args) {
+
+		LOGGER.debug("Hello World !");
+		LOGGER.debug("Welcome to the Public Static Team Qualification Round program :)");
+		
+	}
+
+}
