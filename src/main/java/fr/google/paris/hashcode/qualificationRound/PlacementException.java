e6e09c974626cabf2f87eaf5c4aa67d845e418d3
Luc CLÉMENT <luc.clement421@gmail.com>
2015-03-12 19:54:15 +0100

    Row + PlacementException

---
+++ src/main/java/fr/google/paris/hashcode/qualificationRound/PlacementException.java
@@ -0,0 +1,9 @@
+package fr.google.paris.hashcode.qualificationRound;
+
+public class PlacementException extends Exception {
+	
+	public PlacementException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ src/main/java/fr/google/paris/hashcode/qualificationRound/Row.java
@@ -0,0 +1,62 @@
+package fr.google.paris.hashcode.qualificationRound;
+
+import java.util.HashMap;
+
+public class Row {
+
+	private int id;
+	private int[] occupation;
+	private HashMap<Integer, Server> layout = new HashMap<Integer, Server>();
+	
+	public void addServer(int slot, Server server) throws PlacementException {
+		// Check if it's possible
+		for (int i=0; i<server.getSize(); ++i) {
+			if (occupation[slot+i] != 0) {
+				throw new PlacementException("The server can not be add, the slots are not available.");
+			}
+		}
+		
+		// If we are here then it's ok to add it
+		for (int i=0; i<server.getSize(); ++i) {
+			occupation[slot+i] = 0;
+		}
+		layout.put(slot, server);
+	}
+	
+	public Row(int id, int size, int[] unavailable) {
+		this.id = id;
+		occupation = new int[size];
+		for (int i=0; i < size; ++i) {
+			occupation[i] = 0;
+		}
+		for (int i=0; i<unavailable.length; i++) {
+			occupation[unavailable[i]] = -1;
+		}
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int[] getOccupation() {
+		return occupation;
+	}
+	
+	public void setOccupation(int[] occupation) {
+		this.occupation = occupation;
+	}
+	
+	public HashMap<Integer, Server> getLayout() {
+		return layout;
+	}
+	
+	public void setLayout(HashMap<Integer, Server> layout) {
+		this.layout = layout;
+	} 
+	
+	
+}
